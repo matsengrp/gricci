a26e2bfc3934d07063cc3eee4d4bfde4cc13f976
Michael Landis <mlandis@gmail.com>
2014-09-21 12:31:01 -0700

    add matrix->dict converter for labeling graphs

--- nb-fun.py
+++ nb-fun.py
@@ -16,3 +16,16 @@ def matrix_of_csv(fname):
     with open(fname, 'rb') as csvfile:
         mreader = csv.reader(csvfile, delimiter=',', quotechar="'")
         return Matrix([[int(x) for x in row] for row in mreader])
+
+def mtx_to_dict(m, keys=None):
+    d = {}
+    if keys == None or len(keys) != m.nrows():
+        keys = range(m.nrows())
+    
+    for i in range(m.nrows()):
+        d[keys[i]] = []
+        for j in range(m.ncols()):
+            if m[i][j] != 0:
+                d[keys[i]].append(keys[j])
+
+    return(d)
