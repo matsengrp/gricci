f7953f442bb1d96246573539696e21aadd8ebe2e
Erick Matsen <matsen@fredhutch.org>
2015-01-17 12:41:19 -0800

    gh-1: draft of ric_unif_prior_rw.

--- code.py
+++ code.py
@@ -15,15 +15,11 @@ from sage.all import QQ, ZZ, MixedIntegerLinearProgram, matrix
 from collections import OrderedDict, namedtuple
 
 
-def ric_unif_rw(g, source=0, target=1, pm1=1, pm2=4):
+def ric_unif_prior_rw(g, source=0, target=1):
     """
-    Calculate the Ollivier-Ricci curvature for vertices source and target of
-    graph g under the standard lazy random walk with probability of movement
-    pm=pm1/pm2 (expressed as rational to keep results rational).
-    The results are reported in an ordered tuple including entries 'kappa',
-    which is the coarse Ricci curvature as defined in Ollivier (2009), and
-    'ric', which is kappa divided by pm. This normalized version only depends
-    on the graph.
+    Calculate the coarse Ollivier-Ricci curvature for vertices source and
+    target of graph g under the the Metropolis-Hastings setup for the uniform
+    prior on nodes.
     """
 
     d = OrderedDict.fromkeys(
@@ -42,46 +38,63 @@ def ric_unif_rw(g, source=0, target=1, pm1=1, pm2=4):
             D[j, i] = dist
     ds = g.degree(source)
     dt = g.degree(target)
-    # pm=pm1/pm2 is the probability of movement.
-    pm = float(pm1)/pm2
-    # Matrices are multiplied by mass_denominator in order to be
-    # integer-valued.
-    mass_denominator = pm2*ds*dt
 
-    # Mass distribution at j (multiplied by pm2*ds*dt) of one time-step of a
-    # discrete lazy random walk starting at i.
-    # This is (a multiple of a) random walk $m$ as defined in Ollivier's paper,
-    # where it would be denoted $m_i(j)$.
-    def m(i, j):
-        if i == j:
-            return (pm2-pm1)*ds*dt
-        elif D[i, j] == 1:
-            return pm1*ds*dt/g.degree(relevant_verts[i])
+    # Mass transport from a vertex x works as follows: From x, propose a
+    # uniform neighbor y. Prior MH ratio for proposing a move from x to y is
+    # min[1, (g(y -> x) / g(x -> y))] In our case, g(y -> x) = 1/d(y) and g(x
+    # -> y) = 1/d(x) giving MH ratio min[1, d(y) / d(x)] The amount of mass
+    # moving from x to a neighboring y is min[1, d(y) / d(x)] / d(x). We can
+    # then keep things integral by multiplyng by d(x)^2, such that mass
+    # movement is min[d(x), d(y)], and the amount of mass that stays put is
+    # d(x)^2 - sum_i min[d(x), d(yi)]. For example, no mass stays put if every
+    # neighbor of x has lower degree.
+
+    # In the above, we don't know what x is a priori (it could be either source
+    # or target), so we multiply the amount of mass by the product of both
+    # squares:
+    mass_denominator = ds*ds*dt*dt
+
+    def m(x, y):
+        """
+        This function assumes that x is either source or target.
+        """
+        assert(x == source or x == target)
+        # rn is for "remaining numerator" when we multiply the ratio by
+        # mass_denominator. The rest gets absorbed into the calculation as
+        # described just above.
+        rn = ds*ds if x == target else dt*dt
+        dx = g.degree(x)
+        dy = g.degree(y)
+        if x == y:
+            tot = sum(min(dx, g.degree(z)) for z in g.neighbor_iterator(x))
+            return rn*(dx*dx - tot)
+        elif g.has_edge(x, y):
+            return rn*min(dx, dy)
         else:
             return 0
-
-    # Just handy for taking a look.
-    def mass_vector(i):
-        return [QQ(m(i, j))/mass_denominator for j in range(N)]
 
     # Set up linear program.
     p = MixedIntegerLinearProgram()
     # Note that here and in what follows, i and j are used as the vertices that
     # correspond to relevant_verts[i] and relevant_verts[j].
-    # x[i,j] is the amount of mass that goes from i to j.
+    # a[i,j] is the amount of mass that goes from i to j.
     # It is constrained to be nonnegative, which are the only inequalities for
     # our LP.
-    x = p.new_variable(nonnegative=True)
+    a = p.new_variable(nonnegative=True)
     # Maximize the negative of the mass transport.
     p.set_objective(
-        -p.sum(D[i, j]*x[i, j] for i in range(N) for j in range(N)))
-    # The equality constraints simply state that the mass starts in
-    # m_source, which is relevant_verts[0]...
+        -p.sum(D[i, j]*a[i, j] for i in range(N) for j in range(N)))
+    # The equality constraints simply state that the mass starts in the places
+    # it has diffused to from source...
     for i in range(N):
-        p.add_constraint(p.sum(x[i, j] for j in range(N)) == m(0, i))
-    # and finishes in m_target, which is relevant_verts[1].
+        p.add_constraint(
+            p.sum(a[i, j] for j in range(N)) ==
+            m(source, relevant_verts[i]))
+    # and finishes in the places it has diffused to from target.
     for j in range(N):
-        p.add_constraint(p.sum(x[i, j] for i in range(N)) == m(1, j))
+        p.add_constraint(
+            p.sum(a[i, j] for i in range(N)) ==
+            m(target, relevant_verts[j]))
 
     p.solve()
     W1 = -QQ(p.solve())/mass_denominator
@@ -93,7 +106,7 @@ def ric_unif_rw(g, source=0, target=1, pm1=1, pm2=4):
         verts=relevant_verts,
         coupling={
             k: QQ(v/mass_denominator)
-            for (k, v) in p.get_values(x).items() if v > 0},
+            for (k, v) in p.get_values(a).items() if v > 0},
         dist=D[0, 1],
         W1=W1,
         kappa=kappa,
